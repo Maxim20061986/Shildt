949080c7dccb87c121588ca3856f23a0836d9445
Макс <agostars@gmail.com>
2016-11-22 20:55:48 +0300

    commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package firstPackage;
+
+/**
+ * Created by Макс on 22.11.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
