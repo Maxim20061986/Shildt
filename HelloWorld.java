e368c60027e796a5f50b9081dff8042ee4aeb41f
Макс <agostars@gmail.com>
2016-11-22 21:21:19 +0300

    commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,5 +6,7 @@ package firstPackage;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("commit");
     }
+
 }
