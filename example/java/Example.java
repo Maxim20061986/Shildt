467d67730692d6bd74da61bcb726596495b3cd81
Макс <agostars@gmail.com>
2016-12-08 20:28:31 +0300

    commit

--- example/java/Example.java
+++ example/java/Example.java
@@ -7,6 +7,7 @@ public class Example {
     //main()
     public static void main(String args[]) {
         System.out.println("Простая программа на ява");
-        System.out.println("Еще один комит");
+        System.out.println("Еще один комит" +
+                "");
     }
 }
